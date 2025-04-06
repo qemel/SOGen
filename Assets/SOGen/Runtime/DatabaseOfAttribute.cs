a004e448e7f588471fe82652db2d8fadab8e2143
Noth827 <99727161+Noth827@users.noreply.github.com>
2025-04-07 00:21:55 +0900

    :tada:

---
+++ Assets/SOGen/Runtime/DatabaseOfAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SOGen.Runtime
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DatabaseOfAttribute : Attribute
+    {
+        public Type IDType { get; set; }
+        public Type DataType { get; set; }
+    }
+}
